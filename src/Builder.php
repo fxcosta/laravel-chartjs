2704a70ae046eb7bac0c63fad45f0f9d98121121
pjeutr <github@pjeutr.com>
2017-02-09 09:48:01 +0100

    Update Builder.php
    Fix error

--- src/Builder.php
+++ src/Builder.php
@@ -132,6 +132,7 @@ class Builder
 
         return view('chart-template::chart-template')
                 ->with('datasets', $chart['datasets'])
+                ->with('element', $this->name)
                 ->with('labels', $chart['labels'])
                 ->with('options', isset($chart['options']) ? $chart['options'] : '')
                 ->with('optionsRaw', isset($chart['optionsRaw']) ? $chart['optionsRaw'] : '')
