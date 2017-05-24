52f0d51d30cec75d7711a877ec984cfd56e643f8
fxcosta <fx3costa@gmail.com>
2017-05-23 21:17:55 -0300

    related to #39 issue

--- src/resources/views/chart-template.blade.php
+++ src/resources/views/chart-template.blade.php
@@ -1,20 +1,22 @@
 <canvas id="{!! $element !!}" width="{!! $size['width'] !!}" height="{!! $size['height'] !!}">
 <script>
-    (function() {
-		"use strict";
-        let ctx = document.getElementById("{!! $element !!}");
-        window.{!! $element !!} = new Chart(ctx, {
-            type: '{!! $type !!}',
-            data: {
-                labels: {!! json_encode($labels) !!},
-                datasets: {!! json_encode($datasets) !!}
-            },
-            @if(!empty($optionsRaw))
-                options: {!! $optionsRaw !!}
-            @elseif(!empty($options))
-                options: {!! json_encode($options) !!}
-            @endif
-        });
-    })();
+    document.addEventListener("DOMContentLoaded", function(event) {
+        (function() {
+    		"use strict";
+            let ctx = document.getElementById("{!! $element !!}");
+            window.{!! $element !!} = new Chart(ctx, {
+                type: '{!! $type !!}',
+                data: {
+                    labels: {!! json_encode($labels) !!},
+                    datasets: {!! json_encode($datasets) !!}
+                },
+                @if(!empty($optionsRaw))
+                    options: {!! $optionsRaw !!}
+                @elseif(!empty($options))
+                    options: {!! json_encode($options) !!}
+                @endif
+            });
+        })();
+    });
 </script>
 </canvas>
